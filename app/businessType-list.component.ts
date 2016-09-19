6bcb6def6b969d482c561712a134a02d3aaa48d5
Bjorn Stange <bjorn248@gmail.com>
2016-09-19 17:54:38 -0400

    clean up data collection a little bit, make it more flexible

--- app/businessType-list.component.ts
+++ app/businessType-list.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { BusinessType } from './businessType';
 import { BusinessTypeService } from './businessTypes.service';
-import { BusinessCategory } from './businessCategory';
-import { Permit } from './permit';
 
 @Component({
   selector: 'businesstype-list',
@@ -30,12 +27,9 @@ import { Permit } from './permit';
   `
 })
 export class BusinessTypeListComponent implements OnInit {
-    businessTypes: BusinessType[] = [];
-    permits: Permit[] = [];
-    finalBusinessCategories: BusinessCategory[] = [];
+    finalBusinessCategories: any = [];
     errorMessage: string = '';
     isLoading: boolean = true;
-    businessCategories: any = {};
 
     constructor(private businessTypesService: BusinessTypeService) {}
 
@@ -43,75 +37,8 @@ export class BusinessTypeListComponent implements OnInit {
         this.businessTypesService
         .getAll()
         .subscribe(
-            data => {
-                this.businessTypes = data[0];
-                this.permits = data[1];
-                this.finalBusinessCategories = this.combineCategoriesAndPermits(this.businessTypes, this.permits);
-                this.isLoading = false;
-                this.errorMessage = '';
-            }
-        );
-    }
-
-    combineCategoriesAndPermits(businessTypes: BusinessType[], permits: Permit[]): BusinessCategory[] {
-        // Initialize permit lookup object
-        let permitNames: any = {};
-        // Populate permit lookup object
-        // This prevents us from iterating over the list of permits if we
-        // only want one and allows us to individually add permits from
-        // this object as if it were a lookup table
-        for (let permit of permits) {
-            permitNames[permit.permit_name] = permit;
-        }
-
-        let businessCategories: any = {};
-        // Deduplicate Business Categories
-        for (let businessType of businessTypes) {
-            businessCategories[businessType.business_category] = true;
-        }
-
-        // Initialize array of BusinessCategories to be populated and returned by this function
-        let returnBusinessCategories: BusinessCategory[] = [];
-
-        // Loop through deduplicated businessCategories
-        for (let businessCategory in businessCategories) {
-            if (businessCategories.hasOwnProperty(businessCategory)) {
-                // Initialize empty array of type BusinessType to be used in our BusinessCategory
-                let businessTypesForThisCategory: BusinessType[] = [];
-                let businessTypesWithPermits: BusinessType[] = [];
-                for (let businessType of businessTypes) {
-                    // Find business types that have the same category as the category we are currently on in the parent loop
-                    if (businessType.business_category === businessCategory) {
-                        businessTypesForThisCategory.push(businessType);
-                    }
-                }
-
-                for (let businessTypeForThisCategory of businessTypesForThisCategory) {
-                    let permitsForThisType: Permit[] = [];
-                    for (let permitName in permitNames) {
-                        if (permitNames.hasOwnProperty(permitName)) {
-                            if (businessTypeForThisCategory[permitName] === 'Required') {
-                                permitsForThisType.push(permitNames[permitName]);
-                            }
-                        }
-                    }
-                    let finalBusinessType = <BusinessType>({
-                        business_type: businessTypeForThisCategory.business_type,
-                        business_category: businessTypeForThisCategory.business_category,
-                        permits: permitsForThisType
-                    });
-                    businessTypesWithPermits.push(finalBusinessType);
-                }
-
-                let finalBusinessCategory = <BusinessCategory>({
-                    // name: string
-                    name: businessCategory,
-                    // businessTypes: BusinessType[]
-                    businessTypes: businessTypesWithPermits
-                });
-                returnBusinessCategories.push(finalBusinessCategory);
-            }
-        }
-        return returnBusinessCategories;
+             /* happy path */ b => this.finalBusinessCategories = b,
+             /* error path */ e => this.errorMessage = e,
+             /* onComplete */ () => this.isLoading = false);
     }
 }
