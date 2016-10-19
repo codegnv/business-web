94b569fd8db74c675a2b30e3ef4ab48c4d1cfb13
Reginald Pierre-Jean <Reginald@geoplan.ufl.edu>
2016-10-19 17:27:45 -0400

    pulling in bootstrap and jquery Signed-off-by: Reginald Pierre-Jean <Reginald@geoplan.ufl.edu>

---
+++ app/index.component.ts
@@ -0,0 +1,8 @@
+﻿import { Component } from '@angular/core';
+import * as $ from 'jquery';
+@Component({
+    selector: 'index',
+    templateUrl: './index.component.html'
+})
+export class IndexComponent {
+}
