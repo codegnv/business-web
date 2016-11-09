742d8b6199e7157882864c936a0b1408f4f0b824
Andrew Crites <acrites@mobiquityinc.com>
2016-11-09 10:55:12 -0500

    issue-35: Formatting changes for code style

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,26 +1,25 @@
 import { Routes, RouterModule } from '@angular/router';
-import { HeaderComponent } from './components/header.component';
 import { BusinessTypeListComponent } from './components/businessType-list.component';
 
 // Route config let's you map routes to components
 const routes: Routes = [
-  // map '/businesstypes' to the people list component
-  {
-    path: 'businesstypes',
-    component: BusinessTypeListComponent,
-  },
+    // map '/businesstypes' to the people list component
+    {
+        path: 'businesstypes',
+        component: BusinessTypeListComponent,
+    },
 
-  // map '/' to '/businesstypes' as our default route
-  {
-    path: '',
-    redirectTo: '/businesstypes',
-    pathMatch: 'full'
-  },
-    
-  { 
-    path: '**', 
-    redirectTo: '' 
-  }
+    // map '/' to '/businesstypes' as our default route
+    {
+        path: '',
+        redirectTo: '/businesstypes',
+        pathMatch: 'full'
+    },
+
+    {
+        path: '**',
+        redirectTo: ''
+    },
 ];
 
 export const appRoutingProviders: any[] = [];
--- app/components/businessType-list.component.ts
+++ app/components/businessType-list.component.ts
@@ -3,31 +3,25 @@ import { BusinessTypeService } from '../businessTypes.service';
 import { BusinessCategory } from '../businessCategory';
 import { BusinessType } from '../businessType';
 import {
-  Input,
-  trigger,
-  state,
-  style,
-  transition,
-  animate
+    trigger,
+    style,
+    transition,
+    animate
 } from '@angular/core';
 
 @Component({
-  selector: 'businesstype-list',
-  templateUrl: './businessType-list.component.html',
+    selector: 'businesstype-list',
+    templateUrl: './businessType-list.component.html',
 
-  animations: [
-    trigger('flyInOut', [
-      state('in', style({transform: 'translateX(100)'})),
-      transition('void => *', [
-        style({transform: 'translateX(100%)'}),
-        animate(100)
-      ]),
-      // transition('* => void', [
-      //   animate(100, style({transform: 'translateX(-100%)'}))
-      // ])
-    ])
-  ]
-    
+    animations: [
+        trigger('flyInOut', [
+            transition('void => *', [
+                style({transform: 'translateX(100%)'}),
+                animate(100)
+            ]),
+        ])
+    ]
+
 })
 export class BusinessTypeListComponent implements OnInit {
     finalBusinessCategories: any = [];
@@ -36,18 +30,17 @@ export class BusinessTypeListComponent implements OnInit {
     selectedbusinessCategory: BusinessCategory[] = [];
     selectedbusinesstype: BusinessType[] = [];
 
-
     constructor(private businessTypesService: BusinessTypeService) {}
 
     ngOnInit() {
         this.businessTypesService
         .getAll()
         .subscribe(
-             /* happy path */ b => this.finalBusinessCategories = b,
-             /* error path */ e => this.errorMessage = e,
-             /* onComplete */ () => this.isLoading = false);
+            /* happy path */ b => this.finalBusinessCategories = b,
+            /* error path */ e => this.errorMessage = e,
+            /* onComplete */ () => this.isLoading = false);
     }
-   onSelect(BusinessCategory: BusinessCategory[]): void {
-    this.selectedbusinessCategory = BusinessCategory;      
-  }
+    onSelect(BusinessCategory: BusinessCategory[]): void {
+        this.selectedbusinessCategory = BusinessCategory;
+    }
 }
