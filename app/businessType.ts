1bfa673215c586116f135d0db191dbe31ae970bb
Andrew Crites <acrites@mobiquityinc.com>
2016-11-29 20:24:59 -0500

    issue-81 expand/collapse permit list by business type title

--- app/businessType.ts
+++ app/businessType.ts
@@ -4,6 +4,7 @@ export interface BusinessType {
     readonly business_category: string;
     readonly business_type: string;
     permits: Permit[];
+    collapsed: boolean;
     // Many additional properties on business type that define some details
     // as strings but aren't used by the app in a significant way
     [propName: string]: any;
