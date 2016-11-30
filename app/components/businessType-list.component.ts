66b424025960c0d162c4ebc4b136939a54d0aef1
Reginald Pierre-Jean <Reginald@geoplan.ufl.edu>
2016-11-30 12:32:35 -0500

    issue-37 - Angular2 templating on Business list component HTML - Business list component extended to support templating 	-Next Step (State management and Expand animation) - CSS Styling - fontawesome

--- app/components/businessType-list.component.ts
+++ app/components/businessType-list.component.ts
@@ -6,41 +6,51 @@ import {
     trigger,
     style,
     transition,
+    state,
     animate
 } from '@angular/core';
 
 @Component({
     selector: 'businesstype-list',
     templateUrl: './businessType-list.component.html',
-
+    
     animations: [
         trigger('flyInOut', [
             transition('void => *', [
                 style({transform: 'translateX(100%)'}),
                 animate(100)
             ]),
+        ]),
+         trigger('flyOutIn', [
             transition('* => void', [
-                animate(100, style({transform: 'translateX(100%)'})),
+                style({transform: 'translateX(100%)'}),
+                animate(100)
             ]),
         ]),
-
-        trigger('collapse', [
-            transition('void => *', [
-                style({height: 0}),
-                animate(100, style({height: '*'}))
-            ]),
+        trigger('collapseIn', [
+            state('in', style({height: '*'})),
             transition('* => void', [
-                animate(100, style({height: 0}))
-            ]),
-        ]),
+              style({height: '*'}),
+              animate(400, style({height: 0}))
+            ])
+          ])
     ]
+
 })
 export class BusinessTypeListComponent implements OnInit {
     finalBusinessCategories: any = [];
     errorMessage: string = '';
     isLoading: boolean = true;
+
+    businessCategoryTypesListState:string = 'closed';
+
     selectedbusinessCategory: BusinessCategory[] = [];
-    selectedbusinesstype: BusinessType[] = [];
+    selectedBusinessType: BusinessType[] = [];
+    toggleAllBusinessTypes: boolean = true;
+    togglebusinessCategories: boolean =  true;            
+    toggleSelectedBusinesstype: boolean = false;
+
+    showSelectedBusinesstype: boolean = false;
 
     constructor(private businessTypesService: BusinessTypeService) {}
 
@@ -52,11 +62,26 @@ export class BusinessTypeListComponent implements OnInit {
             /* error path */ e => this.errorMessage = e,
             /* onComplete */ () => this.isLoading = false);
     }
-    onSelect(BusinessCategory: BusinessCategory[]): void {
-        if (this.selectedbusinessCategory === BusinessCategory) {
-            this.selectedbusinessCategory = [];
-        } else {
-            this.selectedbusinessCategory = BusinessCategory;
-        }
+    onClickTogglebusinessCategories(): void {
+        this.togglebusinessCategories = !this.togglebusinessCategories;
     }
+    onSelectBusinessCategory(BusinessCategory: BusinessCategory[]): void { 
+            if ( BusinessCategory !== this.selectedbusinessCategory) {
+                this.showSelectedBusinesstype = true;
+                this.toggleAllBusinessTypes = true;
+                this.toggleSelectedBusinesstype =  false;
+                this.selectedbusinessCategory = BusinessCategory;
+                this.businessCategoryTypesListState = (this.businessCategoryTypesListState === 'closed' ? 'open':'closed');
+            } else {
+                this.selectedbusinessCategory = BusinessCategory;
+                this.toggleAllBusinessTypes = true;
+                this.toggleSelectedBusinesstype =  false;
+                this.showSelectedBusinesstype =  !this.showSelectedBusinesstype;
+                this.businessCategoryTypesListState = (this.businessCategoryTypesListState === 'closed' ? 'open':'closed');                
+            }
+    }  
+     onSelectBusinessCategoryType(BusinessCategoryType: BusinessType[]): void {
+        this.toggleAllBusinessTypes = false;        
+        this.selectedBusinessType = BusinessCategoryType;
+    }   
 }
