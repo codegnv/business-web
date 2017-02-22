264c42519948fd5364076be0ec050d55b7050849
Firoze Rakib <frakib@mobiquityinc.com>
2017-02-21 20:10:31 -0500

    issue-33 searches by category name and business types still to do: search by permit name

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BusinessTypeService } from './businessTypes.service';
+import { BusinessSearchService } from './businessSearch.service';
 
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
-    providers: [BusinessTypeService]
+    providers: [BusinessTypeService, BusinessSearchService]
 })
 export class AppComponent {
     title: string = 'Business Types!';
--- app/components/header.component.ts
+++ app/components/header.component.ts
@@ -1,6 +1,29 @@
 ﻿import { Component } from '@angular/core';
+import { BusinessSearchService } from '../businessSearch.service';
+
 @Component({
     selector: 'header',
     templateUrl: './header.component.html',
 })
-export class HeaderComponent { }
+export class HeaderComponent {
+
+    constructor(private businessSearchService: BusinessSearchService) {}
+
+    searchString = '';
+
+    searchKeyUp(event: any) {
+        // event.keyCode is 13 for 'Enter' Key
+        // which we will detect and begin search
+        if (event.keyCode === 13) {
+            this.businessSearchService.updateValue(this.searchString);
+        } else {
+            if (this.searchString.trim().length === 0) {
+                this.businessSearchService.updateValue('');
+            }
+        }
+    }
+
+    searchBtnClicked() {
+        this.businessSearchService.updateValue(this.searchString);
+    }
+}
