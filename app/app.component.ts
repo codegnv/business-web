77a044ce95387b57497046e2716b48d1925a2733
Bjorn Stange <bjorn248@gmail.com>
2017-10-28 17:35:05 -0400

    issue: #196 fixing webpack svg loading issue

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,6 +8,20 @@ import { BusinessSearchService } from './businessSearch.service';
     providers: [BusinessTypeService, BusinessSearchService]
 })
 export class AppComponent {
+    dreamIcon = require('../images/icons/Dream.svg');
+    planIcon = require('../images/icons/Plan.svg');
+    financeIcon = require('../images/icons/Finance.svg');
+    legalizeIcon = require('../images/icons/Legalize.svg');
+    brandIcon = require('../images/icons/Brand.svg');
+    searchIcon = require('../images/icons/Search.svg');
+    permitIcon = require('../images/icons/Permit.svg');
+    buildIcon = require('../images/icons/Build.svg');
+    hireIcon = require('../images/icons/Hire.svg');
+    taxesIcon = require('../images/icons/Taxes.svg');
+    celebrateIcon = require('../images/icons/Celebrate.svg');
+    openIcon = require('../images/icons/Open.svg');
+    growIcon = require('../images/icons/Grow.svg');
+
     navbarCollapsed = true;
     title: string = 'Business Types!';
     onNavClick(): void {
@@ -17,67 +31,67 @@ export class AppComponent {
         {
             name : 'Dream',
             link: '/steps/1',
-            icon: '/images/icons/dream.svg',
+            icon: this.dreamIcon,
             class: 'stage-yellow'
         }, {
             name : 'Plan',
             link: '/steps/2',
-            icon: '/images/icons/plan.svg',
+            icon: this.planIcon,
             class: 'stage-yellow'
         }, {
             name : 'Finance',
             link: '/steps/3',
-            icon: '/images/icons/finance.svg',
+            icon: this.financeIcon,
             class: 'stage-yellow'
         }, {
             name : 'Legalize',
             link: '/steps/4',
-            icon: '/images/icons/legalize.svg',
+            icon: this.legalizeIcon,
             class: 'stage-yellow'
         }, {
             name : 'Brand',
             link: '/steps/5',
-            icon: '/images/icons/brand.svg',
+            icon: this.brandIcon,
             class: 'stage-yellow'
         }, {
             name : 'Search',
             link: '/steps/6',
-            icon: '/images/icons/search.svg',
+            icon: this.searchIcon,
             class: 'stage-blue'
         }, {
             name : 'Permit',
             link: '/permit-locator',
-            icon: '/images/icons/permit.svg',
+            icon: this.permitIcon,
             class: 'stage-blue'
         }, {
             name : 'Build',
             link: '/steps/8',
-            icon: '/images/icons/build.svg',
+            icon: this.buildIcon,
             class: 'stage-blue'
         }, {
             name : 'Hire',
             link: '/steps/10',
-            icon: '/images/icons/hire.svg',
+            icon: this.hireIcon,
             class: 'stage-red'
         }, {
             name : 'Taxes',
             link: '/steps/11',
-            icon: '/images/icons/taxes.svg',
+            icon: this.taxesIcon,
             class: 'stage-red'
         }, {
             name : 'Celebrate',
             link: '/steps/12',
-            icon: '/images/icons/celebrate.svg',
+            icon: this.celebrateIcon,
             class: 'stage-red'
         }, {
             name : 'Open',
             link: '/steps/13',
-            icon: '/images/icons/open.svg',
+            icon: this.openIcon,
             class: 'stage-red'
         }, {
             name : 'Grow',
             link: '/steps/14',
-            icon: '/images/icons/grow.svg',
+            icon: this.growIcon,
             class: 'stage-red'
         }
     ];
